426f1f8cfe2dbece6b1cbbaa4fb77112af55767e
Fedorenko Serhiy <jobfedserhii@gmail.com>
2024-02-18 08:49:46 +0200

    Complited task without "with"

--- seventh_lesson/deleted_red_line_function.py
+++ seventh_lesson/deleted_red_line_function.py
@@ -1,13 +1,36 @@
 def deleted_line(file_path, del_line):
-    with open(file_path, 'r+') as file:
-        line = file.readlines()
+    try:
+        file = open(file_path, 'r+')
+        lines = file.readlines()
         file.seek(0)
 
-        for i in line:
-          if i.strip() != del_line:
-              file.write(i)
+        for i in lines:
+            if i.strip() != del_line:
+                file.write(i)
 
         file.truncate()
+        file.close()
+    except FileNotFoundError:
+        print('File doesn"n exist!')
+        return None
+    except Exception as e:
+        print('An error occurred while processing files: ', e)
+        return None
+
+
+
+
+
+
+    # with open(file_path, 'r+') as file:
+    #     line = file.readlines()
+    #     file.seek(0)
+    #
+    #     for i in line:
+    #       if i.strip() != del_line:
+    #           file.write(i)
+    #
+    #     file.truncate()
 
 
 deleted_line('my_file.txt', 'И боишься')
--- seventh_lesson/unique_red_line_function.py
+++ seventh_lesson/unique_red_line_function.py
@@ -1,16 +1,35 @@
 def unique_line(first_file, second_file):
-    list1 = []
-    list2 = []
+    try:
+        file1 = open(first_file, 'r')
+        lines1 = file1.readlines()
+        file1.close()
 
-    with open(first_file, 'r') as file1:
-        for line in file1:
-            list1.append(line.strip())
+        file2 = open(second_file, 'r')
+        lines2 = file2.readlines()
+        file2.close()
 
-    with open(second_file, 'r') as file2:
-        for line in file2:
-            list2.append(line.strip())
+        lines1 = [line.strip() for line in lines1]
+        lines2 = [line.strip() for line in lines2]
 
-    return [i for i in list1 if i not in list2]
+        return [line for line in lines1 if line not in lines2]
+
+    except FileNotFoundError:
+        print('File doesn"n exist!')
+        return None
+    except Exception as e:
+        print('An error occurred while processing files: ', e)
+        return None
+
+
+    # with open(first_file, 'r') as file1:
+    #     for line in file1:
+    #         list1.append(line.strip())
+    #
+    # with open(second_file, 'r') as file2:
+    #     for line in file2:
+    #         list2.append(line.strip())
+    #
+    # return [i for i in list1 if i not in list2]
 
 
 
